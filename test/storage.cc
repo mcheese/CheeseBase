583f2919c961a67ca07dff00bfb602601a5bfc75
Cheese <mcheese@users.noreply.github.com>
2015-08-26 01:18:21 +0200

    setup project structure

---
+++ test/storage.cc
@@ -0,0 +1,11 @@
+#include "gtest\gtest.h"
+
+TEST(storage, True)
+{
+  EXPECT_EQ(1, 1);
+}
+
+TEST(storage, False)
+{
+  EXPECT_NE(1, 2);
+}
---
+++ test/test.cc
@@ -0,0 +1,11 @@
+#include "gtest\gtest.h"
+
+TEST(test, Equal)
+{
+  EXPECT_EQ(1, 1);
+}
+
+TEST(test, NotEqual)
+{
+  EXPECT_NE(1, 2);
+}
