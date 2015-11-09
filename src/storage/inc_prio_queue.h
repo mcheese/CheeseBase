a4a2e0d895cdd83fa3b951cb20148e50774f2f8c
Cheese <mcheese@users.noreply.github.com>
2015-11-09 18:50:01 +0100

    implement IncPrioQueue

--- src/storage/inc_prio_queue.h
+++ src/storage/inc_prio_queue.h
@@ -1,6 +1,6 @@
 // Licensed under the Apache License 2.0 (see LICENSE file).
 
-#include <mutex>
+#include <gsl.h>
 #include <vector>
 
 namespace cheesebase {
@@ -10,46 +10,125 @@ namespace cheesebase {
 // extracted. Elements are returned in order of index, the next element to be
 // extracted has the next highest or, if there is no higher one, the very
 // lowest index. It is supposed to optimize the order of disk requests.
+// The structure IS NOT thread-safe! Use external mutex if used parallel.
 template<typename Index, class Value>
 class IncPrioQueue {
 public:
-  IncPrioQueue() = default;
+  IncPrioQueue(size_t reserve = 10)
+  {
+    m_data.reserve(10);
+  }
 
   // Insert element into queue with regards to priority.
   void enqueue(Index index, Value element)
   {
-
+    m_data.emplace_back(index, std::move(element));
+    bubble_up(m_data.size() - 1);
   }
 
   // Get the first element and remove it from the queue.
+  // Queue must NOT be empty before calling.
   Value dequeue()
   {
-    return Value{};
+    Expects(!m_data.empty());
+
+    auto ret = std::move(m_data.front());
+    m_current = ret.first;
+
+    if (m_data.size() > 1) {
+      m_data[0] = std::move(m_data.back());
+      m_data.pop_back();
+      bubble_down(0);
+    } else {
+      m_data.clear();
+    }
+
+    return std::move(ret.second);
   }
 
   // Insert element and get the first from the queue. If the element to be
-  // insertet has the highest priority no manipulation of the queue takes
+  // inserted has the highest priority no manipulation of the queue takes
   // place and it is returned right away.
+  // Queue is allowed to be empty before calling.
   Value exchange(Index index, Value element)
   {
-    return Value{};
+    if (m_data.empty() || compare(index, m_data.front().first)) {
+      return element;
+    } else {
+      auto ret = std::move(m_data.front());
+      m_current = ret.first;
+      m_data[0] = { index, std::move(element) };
+      bubble_down(0);
+      return std::move(ret.second);
+    }
   }
 
   // Return number of elements in the queue.
   size_t size() const
   {
-    return 0;
+    return m_data.size();
   }
 
 private:
   // Returns true if first has a higher or equal priority than second.
   bool compare(Index first, Index second) const
   {
-    return true;
+    if (first >= m_current) {
+      return ((second >= m_current) ? first <= second : true);
+    } else {
+      return ((second >= m_current) ? false : first <= second);
+    }
   }
 
-  Index m_current;
-  std::mutex m_mtx;
+  // Returns parent node position of given pos.
+  static size_t parent(size_t pos)
+  {
+    Expects(pos > 0);
+    return ((pos - 1) / 2);
+  }
+  
+  static std::pair<size_t, size_t> childs(size_t pos)
+  {
+    auto c = pos * 2;
+    return{ c + 1, c + 2 };
+  }
+
+  // Moves the element at pos up the heap to a fitting position.
+  void bubble_up(size_t pos)
+  {
+    Expects(pos < m_data.size());
+    while (pos > 0 && !compare(m_data[parent(pos)].first, m_data[pos].first)) {
+      std::swap(m_data[parent(pos)], m_data[pos]);
+      pos = parent(pos);
+    }
+  }
+  
+  // Moves the element at pos down the heap to a fitting position.
+  void bubble_down(size_t pos)
+  {
+    Expects(pos < m_data.size());
+
+    const auto size = m_data.size();
+    while (true) {
+      auto c = childs(pos);
+      size_t child;
+      if (c.second >= size) {
+        if (c.first >= size) return;
+        child = c.first;
+      } else {
+        child = (compare(m_data[c.first].first, m_data[c.second].first)
+                 ? c.first
+                 : c.second);
+      }
+
+      if (compare(m_data[pos].first, m_data[child].first)) return;
+
+      std::swap(m_data[pos], m_data[child]);
+      pos = child;
+    }
+  }
+
+  Index m_current{ 0 };
   std::vector<std::pair<Index,Value>> m_data;
 };
 
--- test/storage/inc_prio_queue.cc
+++ test/storage/inc_prio_queue.cc
@@ -26,5 +26,74 @@ SCENARIO("Queue and enqueue")
           REQUIRE(q.size() == 0);
       }
     }
+
+    WHEN("many values are inserted")
+    {
+      q.enqueue(10, "10");
+      q.enqueue(12, "12");
+      q.enqueue(5, "5");
+      q.enqueue(7, "7");
+      q.enqueue(1, "1");
+      q.enqueue(8, "8");
+      REQUIRE(q.size() == 6);
+      // 1,5,7,8,10,12
+
+      THEN("they extract in the correct order")
+      {
+        REQUIRE(q.dequeue() == "1");
+        REQUIRE(q.dequeue() == "5");
+        REQUIRE(q.dequeue() == "7");
+        REQUIRE(q.size() == 3);
+
+        AND_WHEN("new values are inserted")
+        {
+          q.enqueue(1, "1");
+          q.enqueue(11, "11");
+          q.enqueue(3, "3");
+          q.enqueue(2, "2");
+          REQUIRE(q.size() == 7);
+          // 8,10,11,12,1,2,3
+
+          THEN("they extract in the correct order")
+          {
+            REQUIRE(q.dequeue() == "8");
+            REQUIRE(q.dequeue() == "10");
+            REQUIRE(q.dequeue() == "11");
+            REQUIRE(q.size() == 4);
+            REQUIRE(q.exchange(12, "12*") == "12*"); // [12*],12,1,2,3
+            REQUIRE(q.exchange(5, "5") == "12");     // [12],1,2,3,5
+            REQUIRE(q.exchange(4, "4") == "1");      // [1],2,3,4,5
+            REQUIRE(q.size() == 4);
+            REQUIRE(q.dequeue() == "2");
+            REQUIRE(q.dequeue() == "3");
+            REQUIRE(q.dequeue() == "4");
+            REQUIRE(q.dequeue() == "5");
+            REQUIRE(q.size() == 0);
+          }
+        }
+      }
+    }
+  }
+  
+  GIVEN("An empty IncPrioQueue with move-only-value")
+  {
+    IncPrioQueue<size_t, std::unique_ptr<std::string>> q{};
+    WHEN("values are inserted")
+    {
+      q.enqueue(0, std::make_unique<std::string>("0"));
+      q.enqueue(5, std::make_unique<std::string>("5"));
+      q.enqueue(3, std::make_unique<std::string>("3"));
+      REQUIRE(q.size() == 3);
+
+      THEN("values can be retrieved in correct order")
+      {
+        REQUIRE(*q.dequeue() == "0");
+        REQUIRE(*q.dequeue() == "3");
+        REQUIRE(*q.exchange(2, std::make_unique<std::string>("2")) == "5");
+        REQUIRE(*q.exchange(1, std::make_unique<std::string>("1")) == "1");
+        REQUIRE(*q.dequeue() == "2");
+        REQUIRE(q.size() == 0);
+      }
+    }
   }
 }
