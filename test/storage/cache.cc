25dfb8dcfd5a7102125e97a9f1075f3273c0548a
Cheese <mcheese@users.noreply.github.com>
2015-11-24 16:04:07 +0100

    add simple Cache test case

--- test/storage/cache.cc
+++ test/storage/cache.cc
@@ -9,5 +9,34 @@ SCENARIO("Reading and writing to cache.")
   GIVEN("A cache")
   {
     Cache cache{ "test.db", OpenMode::create_always, 8 };
+    
+    WHEN("data is written to a page")
+    {
+      const PageNr page{ 5 };
+      const size_t offset{ 100 };
+      const std::string test{ "ABCDEFGHIJKLMNOP" };
+      {
+        auto p = cache.write(page);
+        std::copy(test.begin(), test.end(), p.get_write().data() + offset);
+      }
+      
+      THEN("same data can be read")
+      {
+        auto p = cache.read(page);
+        REQUIRE(std::equal(test.begin(), test.end(), p.get().data() + offset));
+      }
+
+      AND_WHEN("many other pages are read")
+      {
+        for (PageNr i = 0; i < 20; ++i)
+          cache.read(i);
+
+        THEN("same data can sill be read")
+        {
+          auto p = cache.read(page);
+          REQUIRE(std::equal(test.begin(), test.end(), p.get().data() + offset));
+        }
+      }
+    }
   }
 }
