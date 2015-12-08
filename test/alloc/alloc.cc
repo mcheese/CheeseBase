29262a66bf89c72e48efebf7d99d0896c7f331ca
Cheese <mcheese@users.noreply.github.com>
2015-12-08 18:37:51 +0100

    add Allocator test

---
+++ test/alloc/alloc.cc
@@ -0,0 +1,110 @@
+#ifdef _MSC_VER
+#define _SCL_SECURE_NO_WARNINGS
+#endif
+#include "alloc/allocator.h"
+#include "catch.hpp"
+#include "common/common.h"
+#include "storage/storage.h"
+
+using namespace cheesebase;
+
+const Addr eof_addr = offsetof(DskDatabaseHdr, end_of_file);
+const Addr pg_addr = offsetof(DskDatabaseHdr, free_alloc_page);
+const Addr t1_addr = offsetof(DskDatabaseHdr, free_alloc_t1);
+const Addr t2_addr = offsetof(DskDatabaseHdr, free_alloc_t2);
+const Addr t3_addr = offsetof(DskDatabaseHdr, free_alloc_t3);
+const Addr t4_addr = offsetof(DskDatabaseHdr, free_alloc_t4);
+
+const size_t pg_block = k_page_size - 50;
+const size_t t1_block = k_page_size / 2 - 50;
+const size_t t2_block = k_page_size / 4 - 50;
+const size_t t3_block = k_page_size / 8 - 50;
+const size_t t4_block = k_page_size / 16 - 50;
+
+bool contains(const std::vector<Write>& ws, Addr addr, uint64_t word) {
+  for (const auto& w : ws) {
+    if (w.addr == addr && w.data == gsl::as_bytes(gsl::span<uint64_t>(word)))
+      return true;
+  }
+  return false;
+}
+
+SCENARIO("alloc") {
+  GIVEN("an empty Allocator and storage") {
+
+    DskDatabaseHdr hdr;
+    memset(&hdr, 0, sizeof(hdr));
+    hdr.end_of_file = k_page_size;
+    Storage store{ "test.db", OpenMode::create_always };
+    store.storeWrite(
+        Write({ 0, gsl::as_bytes(gsl::span<DskDatabaseHdr>(hdr)) }));
+
+    Allocator alloc{ hdr, store };
+
+    WHEN("blocks are allocated") {
+      auto t = alloc.startTransaction();
+
+      auto b1 = t.alloc(t4_block);
+      auto b2 = t.alloc(pg_block);
+      auto b3 = t.alloc(t1_block);
+      auto writes = t.commit();
+
+      THEN("the blocks and writes are as expected") {
+        // layout
+        //
+        //   [~~~~~~~~~~~~~~~~~~~~~~~~~~ reserved ~~~~~~~~~~~~~~~~~~~~~~~~~~]
+        //   [b1][  ][      ][              ][:::::::::::::: b3 ::::::::::::]
+        //   [::::::::::::::::::::::::::::: b2 :::::::::::::::::::::::::::::]
+        //
+        REQUIRE(b1.size >= t4_block);
+        REQUIRE(b1.addr == k_page_size);
+        REQUIRE(b2.size >= pg_block);
+        REQUIRE(b2.addr == k_page_size * 2);
+        REQUIRE(b3.size >= t1_block);
+        REQUIRE(b3.addr == k_page_size * 1.5);
+
+        REQUIRE(contains(writes, t4_addr, k_page_size + k_page_size / 16));
+        REQUIRE(contains(writes, t3_addr, k_page_size + k_page_size / 8));
+        REQUIRE(contains(writes, t2_addr, k_page_size + k_page_size / 4));
+        REQUIRE(contains(writes, t1_addr, 0));
+        REQUIRE(contains(writes, eof_addr, k_page_size * 3));
+
+        AND_WHEN("more blocks are allocated") {
+          store.storeWrite(writes);
+          t.end();
+          t = alloc.startTransaction();
+
+          auto b4 = t.alloc(t4_block);
+          auto b5 = t.alloc(t2_block);
+          t.free(b1.addr);
+          t.free(b3.addr);
+          auto b6 = t.alloc(t2_block);
+          auto b7 = t.alloc(t1_block);
+          writes = t.commit();
+
+          THEN("the blocks and writes are as expected") {
+            // layout
+            //
+            // [~~~~~~~~~~~~~~~~~~~~~~~~~~ reserved ~~~~~~~~~~~~~~~~~~~~~~~~~~]
+            // [  ][b4][      ][::::: b5 :::::][::::: b6 :::::][              ]
+            // [::::::::::::::::::::::::::::: b2 :::::::::::::::::::::::::::::]
+            // [::::::::::::: b7 :::::::::::::][                              ]
+            //
+
+            REQUIRE(b5.addr == k_page_size + k_page_size / 4);
+            REQUIRE(b6.addr == k_page_size + k_page_size / 2);
+            REQUIRE(b7.addr == k_page_size * 3);
+
+            REQUIRE(contains(writes, t4_addr, k_page_size));
+            REQUIRE(contains(writes, t2_addr,
+                             k_page_size + k_page_size / 2 + k_page_size / 4));
+            REQUIRE(
+                contains(writes, t1_addr, k_page_size * 3 + k_page_size / 2));
+            REQUIRE(contains(writes, eof_addr, k_page_size * 4));
+          }
+          store.storeWrite(writes);
+        }
+      }
+    }
+  }
+}
