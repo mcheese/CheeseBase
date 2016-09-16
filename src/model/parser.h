a4abeca92e73164a0b772b8b1532c04887aa09d8
Cheese <mcheese@users.noreply.github.com>
2016-09-16 16:52:35 +0200

    Rename bool parser to avoid typename conflict

--- src/model/parser.h
+++ src/model/parser.h
@@ -18,7 +18,7 @@ namespace parser {
 const x3::rule<class null, model::Null> null = "null";
 const x3::rule<class missing, model::Missing> missing = "missing";
 const x3::rule<class number, model::Number> number = "number";
-const x3::rule<class boolean, model::Bool> boolean = "boolean";
+const x3::rule<class bool_val, model::Bool> bool_val = "bool_val";
 const x3::rule<class string, model::String> string = "string";
 const x3::rule<class scalar, model::Value> scalar = "scalar";
 const x3::rule<class tuple_mem, model::Tuple_member> tuple_mem = "tuple_mem";
@@ -32,10 +32,10 @@ const x3::rule<class value, model::Value> value = "value";
 const auto null_def = x3::lit("null") >> x3::attr(model::Null());
 const auto missing_def = x3::lit("missing") >> x3::attr(model::Missing());
 const auto number_def = x3::double_;
-const auto boolean_def = x3::lit("true") >> x3::attr(true) |
+const auto bool_val_def = x3::lit("true") >> x3::attr(true) |
                          "false" >> x3::attr(false);
 const auto string_def = x3::lexeme['"' >> *(~x3::char_('"')) >> '"'];
-const auto scalar_def = missing | null | number | boolean | string;
+const auto scalar_def = missing | null | number | bool_val | string;
 const auto tuple_mem_def = string > ':' > value;
 const auto tuple_val_def = x3::lit('{') > -(tuple_mem % ',') > '}';
 const auto collection_def = x3::lit('[') > -(value % ',') > ']';
@@ -43,7 +43,7 @@ const auto value_def = tuple_val | collection | scalar;
 
 ///////////////////////////////////////////////////////////////////////////////
 
-BOOST_SPIRIT_DEFINE(null, missing, number, boolean, string, scalar, tuple_mem,
+BOOST_SPIRIT_DEFINE(null, missing, number, bool_val, string, scalar, tuple_mem,
                     tuple_val, collection, value);
 
 } // namespace parser
